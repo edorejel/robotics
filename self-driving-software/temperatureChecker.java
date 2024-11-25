2f25b89df9634630b49ff2f2ea05c0fcd2efb2cc
edorejel <110068017+edorejel@users.noreply.github.com>
2024-11-24 21:47:56 -0800

    Create temperatureChecker.java

---
+++ self-driving-software/temperatureChecker.java
@@ -0,0 +1,9 @@
+/*
+Convert a temperature checker into a rudimentary "AI" self-driving software application.
+Do not get too fancy with this, it is not difficult.
+Hints: 
+(1) the streets are well maintained
+(2) We're on planet Earth.
+(3) Use the weather, so your app is really "fancy." 
+
+*/
