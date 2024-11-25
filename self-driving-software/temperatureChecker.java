885776dee2741a11a0f36f19c0d5d710563f9fe6
edorejel <110068017+edorejel@users.noreply.github.com>
2024-11-24 22:05:04 -0800

    Update temperatureChecker.java

--- self-driving-software/temperatureChecker.java
+++ self-driving-software/temperatureChecker.java
@@ -2,10 +2,10 @@
 Convert a temperature checker into a rudimentary "AI" self-driving software application.
 Do not get too fancy with this, it is not difficult.
 Hints: 
-(1) The streets are well maintained.
+(1) The streets are well maintained (we're talking exquisitely maintained streets). 
 (2) We're on planet Earth.
 (3) Use the weather, so your app is really "fancy."
-(4) Think about tsunami's and floods, what would that do?
+(4) Think about tsunami's, heavy rains, fog, and floods, what would that do? However, we're on planet Earth, so always keep that in mind. 
 (5) Think about global warming, how would that affect your self-drive AI?
 
 */
