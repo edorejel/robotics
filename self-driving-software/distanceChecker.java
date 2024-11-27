596f60233ec5a970c81012db511cc1267f67c615
edorejel <110068017+edorejel@users.noreply.github.com>
2024-11-27 00:41:13 -0800

    Create distanceChecker.java

---
+++ self-driving-software/distanceChecker.java
@@ -0,0 +1,7 @@
+/*
+Create a distance checker to determine the distance of objects as you drive.
+Identify some objects that are commonly found as a vehicle moves.
+Output the names of the objects, and the distance that they were detected at.
+
+
+*/
